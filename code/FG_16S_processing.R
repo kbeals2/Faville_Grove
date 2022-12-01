0e47c8828461be9460043c267b890feef588f8e5
kbeals2 <kbeals2@vols.utk.edu>
2022-12-01 12:15:14 -0800

    Testing if I can push

--- code/FG_16S_processing.R
+++ code/FG_16S_processing.R
@@ -1,3 +1,5 @@
 #### 16S AMPLICON PROCESSING FOR FAVILLE GROVE PROJECT ####
 # Soil samples collected from Faville Prairie (remnant) and Tillotson Prairie (restoration) in summer 2022
 # Used the Earth Microbiome Project primers: 515F/806R
+
+# Testing 
